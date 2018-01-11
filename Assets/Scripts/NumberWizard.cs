c1dc915536539121e9a3d092278afe484284070b
Ravi Chhawal <zubin.engg@gmail.com>
2018-01-11 10:36:46 +0530

    test 11

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		print ("Welcome to NUmber Wizard..........");	
+		print ("Pick a number");
+	}
+	
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
